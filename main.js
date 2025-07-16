0310442c97bf9767263952cc91ee92eb6b01bf7b
letangileri <let.angileri@gmail.com>
2025-07-16 12:36:58 +0200

    update

--- main.js
+++ main.js
@@ -2,30 +2,33 @@
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
 
-//Tools
+//Steps
 //- funzione isPalindromo con un parametro (parola inserita dall'utente)
 //- es. parola palindroma ingegni
     //- for per scorrere la sequenza di caratteri dal primo elemento
     //- for per scorrere la sequenza di caratteri dall'ultimo elemento
         //-confrontare se l'ultimo elemento è uguale al primo e così e aggiungere il carattere all'interno di una stringa vuota dichiarata all'inizio
 
-
+// controllo se la parola inserita dall'utente è uguale alla stringa creata all'interno del secondo for
 
 function isPalindromo(word){
     let newWordIsPalindroma = "";
     for (let i = 0; i < word.length; i++) {
         const elementFormZero = word[i];
+        console.log("index i " + i);
         console.log("elementFormZero " + elementFormZero);
         for (let j = word.length-1; j > 0; j--){
             const elementFromLength = word[j];
+            console.log("index j " + j);
             console.log("elementFromLength " + elementFromLength);
             if (elementFromLength === elementFormZero){
                 newWordIsPalindroma += elementFromLength;
-                // console.log("newWord dentro if" + newWord);
+                console.log("newWordIsPalindroma " + newWordIsPalindroma);
+                console.log("DA CAPO");
+                
                 break;
             }
-        }
-        
+        }  
     }
     if (word === newWordIsPalindroma){
         return true;
